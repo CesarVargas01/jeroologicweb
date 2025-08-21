a456114c10e1459b7118d05f8f145899ac12719d
CesarVargas1 <cesaraugustovargasrubiano@gmail.com>
2025-08-20 21:56:43 -0500

    feat: Configure Astro for Vercel deployment and server-side rendering
    This commit configures the Astro project for deployment to Vercel and enables server-side rendering for API routes.
    
    - Installs and integrates the `@astrojs/vercel` adapter.
    - Sets the `output` mode to `server` in `astro.config.mjs`.
    - Updates `package.json` and `package-lock.json` to reflect new dependency.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from "astro-icon";
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   site: 'https://www.jerroologic.com',
   integrations: [
     tailwind(), 
@@ -13,7 +15,8 @@ export default defineConfig({
       changefreq: 'weekly',
       priority: 0.7,
       lastmod: new Date(),
-    })
+    }),
+    vercel()
   ],
   vite: {
     define: {
